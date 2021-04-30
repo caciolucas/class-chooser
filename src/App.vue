44240e5d9ccae56d076ec4b8d09827098ce35525
Cácio Lucas <cclucas060901@gmail.com>
2021-04-30 07:52:11 -0300

    Project started

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,92 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <DataTable
+    :value="turmas"
+    rowGroupMode="subheader"
+    groupRowsBy="nome-componente"
+    scrollable
+    scrollHeight="80vh"
+  >
+    <Column field="codigo-turma" header="Código" style="width: 2rem"></Column>
+    <Column field="capacidade-aluno" header="Capacidade"></Column>
+    <Column field="local" header="Local"></Column>
+    <Column field="ano" header="Ano"></Column>
+    <Column field="periodo" header="Período"></Column>
+    <Column field="periodo" header="Período"></Column>
+    <template #groupheader="slotProps">
+      <span class="fas fa-book"></span>
+      <span class="image-text p-ml-2">
+        {{ slotProps.data["codigo-componente"] }}-{{
+          slotProps.data["nome-componente"]
+        }}</span
+      >
+    </template>
+  </DataTable>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      turmas: {},
+    };
+  },
+  mounted() {
+    const env = process.env;
+    const id_imd = 6069;
+    axios
+      .post(
+        `${env.VUE_APP_URL_BASE}/authz-server/oauth/token?client_id=${env.VUE_APP_CLIENT_ID}&client_secret=${env.VUE_APP_CLIENT_SECRET}&grant_type=client_credentials`
+      )
+      .then((response) => {
+        const token = response.data.access_token;
+        let config = {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "x-api-key": `${env.VUE_APP_X_API_KEY}`,
+          },
+        };
+        // axios
+        //   .get(
+        //     `${env.VUE_APP_URL_BASE}docente/v1/docentes`,
+        //     config
+        //   )
+        axios
+          .get(
+            `${env.VUE_APP_URL_BASE}turma/v1/turmas/57666124/`,
+            config
+          ).then((response) => {
+            console.log(response)
+          })
+        axios
+          .get(
+            `${env.VUE_APP_URL_BASE}turma/v1/turmas?ano=2021&limit=100&periodo=1&id-unidade=${id_imd}`,
+            config
+          )
+          .then((response) => {
+            this.turmas = response.data;
+            console.log(this.turmas[0]);
+            axios.get(
+              `${env.VUE_APP_URL_BASE}turma/v1/turmas/${this.turmas[0]["id-turma"]}`,
+              config
+            ).then((response) => {
+              console.log(response.data['id-docente'])
+            });
+          });
+      });
+    // imd =  requests.get(f'{URL_BASE}/unidade/v1/unidades?sigla=imd',headers=custom_headers).json()[0]
+    // id_imd = imd['id-unidade']
+    // turmas = requests.get(f'{URL_BASE}/turma/v1/turmas?ano=2020&periodo=1&id-unidade={id_imd}',headers=custom_headers).json()
+  },
+};
 </script>
 
 <style>
+body {
+  background: var(--surface-a);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,53 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import 'primevue/resources/themes/arya-blue/theme.css'       //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css'     //icons
+import 'primeflex/primeflex.css';
+
+// import '@fortawesome/fontawesome-free'
+// import '@fortawesome/fontawesome-svg-core'
+// import '@fortawesome/vue-fontawesome'
+
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import Card from 'primevue/card';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Button from 'primevue/button';
+import Timeline from 'primevue/timeline';
+import TabView from 'primevue/tabview';
+import TabPanel from 'primevue/tabpanel';
+import Dialog from 'primevue/dialog';
+import Toolbar from 'primevue/toolbar';
+import InputText from 'primevue/inputtext';
+import Fieldset from 'primevue/fieldset';
+import ToastService from 'primevue/toastservice';
+import Toast from 'primevue/toast';
+
+const app = createApp(App);
+
+app.use(PrimeVue)
+app.use(VueAxios, axios) // 👈
+app.use(ToastService) // 
+
+
+app.component('Card', Card);
+app.component('DataTable', DataTable);
+app.component('Column', Column);
+app.component('Button', Button);
+app.component('Timeline', Timeline);
+app.component('TabView', TabView);
+app.component('TabPanel', TabPanel);
+app.component('Dialog', Dialog);
+app.component('Toolbar', Toolbar);
+app.component('InputText', InputText);
+app.component('Fieldset', Fieldset);
+app.component('Toast', Toast);
+
+app.mount('#app')
